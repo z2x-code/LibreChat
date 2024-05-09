64cda9be8d0d882bfe28c978e93d7f788cec3ef5
Z2X <dxsoft@foxmail.com>
2024-05-09 16:12:30 +0800

    change text

--- client/src/localization/languages/Zh.ts
+++ client/src/localization/languages/Zh.ts
@@ -324,7 +324,7 @@ export default {
   com_endpoint_skip_hover: '跳过补全步骤， 检查最终答案和生成步骤',
   com_endpoint_config_key: '设置API Key',
   com_endpoint_assistant_placeholder: '请从右侧面板中选择助手',
-  com_endpoint_config_placeholder: '扫码加微信让客服帮您开通使用权限或者直接升级为 VIP 使用',
+  com_endpoint_config_placeholder: '扫码加客服微信帮您开通使用权限或者直接升级为 VIP 使用',
   com_endpoint_config_key_for: '设置API Key：',
   com_endpoint_config_key_name: '密钥',
   com_endpoint_config_value: '输入值：',
